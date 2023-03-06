c3f95ac7e3113ef1b835f32f4d4b72f4a98f9f39
DatGit <kieuducdat2k@gmail.com>
2023-03-06 20:52:49 +0700

    MovieApp code bằng ASP.NET core MVC

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+
+        //public string Welcome(string name, int age = 1, int ID = 3)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name},age: {age}, id is: {ID}");
+        //}
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
